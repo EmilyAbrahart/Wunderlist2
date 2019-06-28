fb16666ced75b60057da7e5f00a5e7c8f57c9406
EmilyAbrahart <emilyabrahart@gmail.com>
2019-06-28 04:59:29 +0100

    Spinner

--- wunderlist/src/components/private-components/ActiveTodos.jsx
+++ wunderlist/src/components/private-components/ActiveTodos.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { updateTodo } from '../../state/actions';
 import Todo from './Todo';
 import styled from 'styled-components';
-import { FlexFunc, tablet } from '../../styles';
+import { FlexFunc, tablet, color_dark } from '../../styles';
 
 const TodoListDiv = styled.div`
 	${FlexFunc('column', 'center', 'flex-start')};
@@ -13,17 +13,66 @@ const TodoListDiv = styled.div`
 	margin: 0 auto;
 	box-sizing: border-box;
 
-	@media ${tablet}{
+	@media ${tablet} {
 		${FlexFunc('row', 'center', 'center')};
 		flex-wrap: wrap;
 	}
 `;
 
+const Spinner = styled.div`
+position: absolute;
+top: 50%;
+left: 50%;
+.lds-ring {
+  display: inline-block;
+  position: relative;
+  width: 64px;
+  height: 64px;
+}
+.lds-ring div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 51px;
+  height: 51px;
+  margin: 6px;
+  border: 6px solid ${color_dark};
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: ${color_dark} transparent transparent transparent;
+}
+.lds-ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+.lds-ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+.lds-ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+@keyframes lds-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+`
 const ActiveTodos = props => {
 	return (
 		<TodoListDiv isFiltering={props.isFiltering}>
+	
 			{props.isFetching ? (
-				<div>Loading</div>
+				<Spinner>
+					<div class="lds-ring">
+						<div />
+						<div />
+						<div />
+						<div />
+					</div>
+				</Spinner>
 			) : !props.todos || props.todos.length === 0 ? (
 				<div>You have no active tasks</div>
 			) : (
